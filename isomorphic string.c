9a33db5b393da4b709b1e417ca9838270eea1cc3
vaishnavidevisgif27 <vaishnavis142005@gmail.com>
2026-03-22 21:04:37 +0530

    Add files via upload

---
+++ isomorphic string.c
@@ -0,0 +1,14 @@
+bool isIsomorphic(char* s, char* t) {
+    int map_s[256] = {0};
+    int map_t[256] = {0};
+    int len = strlen(s);
+
+    for (int i = 0; i < len; i++) {
+        if (map_s[s[i]] != map_t[t[i]]) {
+            return false;
+        }
+        map_s[s[i]] = i + 1;
+        map_t[t[i]] = i + 1;
+    }
+    return true;
+}
