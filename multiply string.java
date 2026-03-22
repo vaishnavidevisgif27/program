af05c90d6efcef0d25e6cf537932f2c138d0d913
vaishnavidevisgif27 <vaishnavis142005@gmail.com>
2026-03-22 21:04:00 +0530

    Add files via upload

---
+++ multiply string.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public String multiply(String num1, String num2) {
+        
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        
+        int m = num1.length(), n = num2.length();
+        int[] result = new int[m + n]; 
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = mul + result[i + j + 1]; 
+
+                result[i + j + 1] = sum % 10; 
+                result[i + j] += sum / 10; 
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) { 
+                sb.append(num);
+            }
+        }
+        
+        return sb.toString(); 
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        
+        System.out.println(solution.multiply("2", "3")); 
+        System.out.println(solution.multiply("123", "456")); 
+        System.out.println(solution.multiply("0", "123")); 
+        System.out.println(solution.multiply("999", "999")); 
+    }
+}
