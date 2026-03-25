e72a14587d7e0c1e554e5c35bb5cbccb777dad39
vaishnavidevisgif27 <vaishnavis142005@gmail.com>
2026-03-25 10:32:58 +0530

    Add files via upload

---
+++ Merge sort.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    struct ListNode dummy;
+    struct ListNode *tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val <= l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = (l1 != NULL) ? l1 : l2;
+    return dummy.next;
+}
